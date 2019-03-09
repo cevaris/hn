c5aa5559d7b6a8e1e77de639464377d881212917
Adam Cardenas <cevaris@gmail.com>
2019-03-09 10:15:52 -0800

    failed attempt at observable infinite list

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, from, Subscription, of } from 'rxjs';
-import { map, mergeMap, flatMap, switchMap } from 'rxjs/operators';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable, from, Subscription, of, BehaviorSubject, zip } from 'rxjs';
+import { map, mergeMap, flatMap, switchMap, tap } from 'rxjs/operators';
 import { HnDatastore, Item } from '../datastore/hn.datastore';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -9,22 +10,14 @@ import { HnDatastore, Item } from '../datastore/hn.datastore';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
+
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
   feed: Observable<number[]>;
   items: Observable<number[]>;
+
+  pages: Observable<number>;
+  currPage: BehaviorSubject<number>;
 
   constructor(private datastore: HnDatastore) { }
 
@@ -40,33 +33,52 @@ export class ListPage implements OnInit {
       });
 
     this.feed = this.datastore.getTopStories();
-
-    this.items = this.feed
-      .pipe(
-        map(itemIds => {
-          console.log('slicing page');
-          return itemIds.slice(0, 30);
-        })
-        // mergeMap(itemId => {
-        //   return <Observable<Item>>this.datastore.getItem(itemId);
-        // }),
-        // flatMap(itemIds => {
-        //   return itemIds.map(this.datastore.getItem;
-        // })
-      )
-      // .subscribe(numbers => {
-      //   console.log('setting numbers');
-      //   this.items = numbers;
-      // })
-    // .subscribe(item => {
-    //   this.items.push(item);
-    //   // reorder because mergeMap executes in parellel
-    //   this.items.sort((a, b) => {
-    //     if (a.id < b.id) return -1;
-    //     if (a.id > b.id) return 1;
-    //   });
-    // });
+    this.pages = this.feed.pipe(
+      map(arr => Math.floor(arr.length / 30) + 1)
+    );
+    // kick off first page load
+    this.currPage = new BehaviorSubject(0);
+    this.currPage
+    .pipe(
+      switchMap(page => {
+        console.log('currPage', page);
+        return this.feed.pipe(
+          map(itemIds => {
+            const nextIds = itemIds.slice((page - 1) * 30, (page - 1) * 30 + 30);
+            console.log('nextIds', nextIds);
+            return nextIds;
+          })
+        )
+      })
+    );
+    this.currPage.next(1);
+    // this.items = this.feed
+    //   .pipe(
+    //     map(itemIds => {
+    //       console.log('slicing page');
+    //       return itemIds.slice(0, 30);
+    //     })
+    //   )
   }
+
+  loadData(event) {
+    console.log('loadData fired', event);
+    setTimeout(() => {
+      this.currPage
+        .next(this.currPage.value + 1);
+
+      console.log('Done');
+      event.target.complete();
+
+      zip(this.pages, this.currPage)
+        .subscribe(([pages, currPage]) => {
+          if (currPage >= pages) {
+            event.target.disabled = true;
+          }
+        });
+    }, 500);
+  }
+
   // add back when alpha.4 is out
   // navigate(item) {
   //   this.router.navigate(['/list', JSON.stringify(item)]);
